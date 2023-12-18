10d8465af49e674804c31f20b22bcc91a17b5adb
devitoDbug <davidOchiengy@gmail.com>
2023-12-18 13:54:00 +0300

    chore: Create entry and transfer sql queries

--- db/query/account.sql
+++ db/query/account.sql
@@ -10,8 +10,10 @@ RETURNING *;
 
 
 -- name: GetAccount :one
-SELECT * FROM accounts
-WHERE id = $1 LIMIT 1;
+SELECT *
+FROM accounts
+WHERE id = $1
+LIMIT 1;
 
 -- name: ListAccounts :many
 SELECT * FROM accounts
