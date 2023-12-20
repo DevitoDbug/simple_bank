fa890cd452009012547aa4f369f8ad4b0001ad63
devitoDbug <davidOchiengy@gmail.com>
2023-12-20 21:02:53 +0300

    test: add unit tests for CRUD operations on account table

--- Makefile
+++ Makefile
@@ -12,4 +12,6 @@ migratedown:
 	migrate -path db/migration -database "postgresql://root:j1751502021@localhost:5432/simple_bank?sslmode=disable" -verbose down
 sqlc:
 	sqlc generate
+test:
+	go test -v -cover ./...
 .PHONY: postgres removepostgres createdb dropdb migrateup migratedown sqlc
--- db/sqlc/account_test.go
+++ db/sqlc/account_test.go
@@ -2,12 +2,13 @@ package db
 
 import (
 	"context"
+	"database/sql"
 	"github.com/stretchr/testify/require"
 	"simple_bank/util"
 	"testing"
 )
 
-func TestCreateAccount(t *testing.T) {
+func createRandomAccount(t *testing.T) Account {
 	arg := CreateAccountParams{
 		Owner:    util.RandomOwner(),
 		Balance:  util.RandomMoney(),
@@ -24,4 +25,78 @@ func TestCreateAccount(t *testing.T) {
 
 	require.NotZero(t, account.ID) //Check if the id is set
 	require.NotZero(t, account.CreatedAt)
+
+	return account
+}
+
+func TestCreateAccount(t *testing.T) {
+	createRandomAccount(t)
+}
+
+func TestGetAccount(t *testing.T) {
+	createdAccount := createRandomAccount(t)
+
+	fetchedAccount, err := testQueries.GetAccount(context.Background(), createdAccount.ID)
+
+	require.NoError(t, err)
+	require.NotEmpty(t, fetchedAccount)
+
+	require.Equal(t, createdAccount.ID, fetchedAccount.ID)
+	require.Equal(t, createdAccount.CreatedAt, fetchedAccount.CreatedAt)
+	require.Equal(t, createdAccount.Owner, fetchedAccount.Owner)
+	require.Equal(t, createdAccount.Currency, fetchedAccount.Currency)
+	require.Equal(t, createdAccount.Balance, fetchedAccount.Balance)
+}
+
+func TestDeleteAccount(t *testing.T) {
+	createdAccount := createRandomAccount(t)
+
+	err := testQueries.DeleteAccount(context.Background(), createdAccount.ID)
+	require.NoError(t, err)
+
+	//Check if it is still in the db
+	account2, err := testQueries.GetAccount(context.Background(), createdAccount.ID)
+	require.Error(t, err)
+	require.EqualError(t, err, sql.ErrNoRows.Error())
+	require.Empty(t, account2)
+}
+
+func TestUpdateAccount(t *testing.T) {
+	createdAccount := createRandomAccount(t)
+
+	arg := UpdateAccountParams{
+		ID:      createdAccount.ID,
+		Balance: util.RandomMoney(),
+	}
+
+	updatedAccount, err := testQueries.UpdateAccount(context.Background(), arg)
+	require.NoError(t, err)
+	require.NotEmpty(t, updatedAccount)
+
+	require.Equal(t, createdAccount.ID, updatedAccount.ID)
+	require.Equal(t, createdAccount.CreatedAt, updatedAccount.CreatedAt)
+	require.Equal(t, createdAccount.Owner, updatedAccount.Owner)
+	require.Equal(t, createdAccount.Currency, updatedAccount.Currency)
+	require.Equal(t, updatedAccount.Balance, arg.Balance)
+}
+
+func TestListAccounts(t *testing.T) {
+	for i := 0; i < 10; i++ {
+		createRandomAccount(t)
+	}
+
+	args := ListAccountsParams{
+		Limit:  5,
+		Offset: 5,
+	}
+
+	accounts, err := testQueries.ListAccounts(context.Background(), args)
+	require.NoError(t, err)
+	require.NotEmpty(t, accounts)
+	require.Len(t, accounts, 5)
+
+	//Check each account if it is empty
+	for _, account := range accounts {
+		require.NotEmpty(t, account)
+	}
 }
