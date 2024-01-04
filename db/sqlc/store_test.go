a2ba2af79ff73cf84776c34be51e49ff6ce5f936
devitoDbug <davidOchiengy@gmail.com>
2024-01-04 21:44:03 +0300

    fix: fixed some bugs with the money transfer test

--- db/sqlc/store_test.go
+++ db/sqlc/store_test.go
@@ -36,4 +36,39 @@ func TestTransferTx(t *testing.T) {
 	account1 := createRandomAccountForTransaction(t)
 	account2 := createRandomAccountForTransaction(t)
 
+	transferResults := make(chan TransferTxResult)
+	transferResultsError := make(chan error)
+
+	noOfTransfers := 5
+	amount := int64(10)
+	for i := 0; i < noOfTransfers; i++ {
+		go func() {
+			//we cannot check the tests here because it is in a separate go routine form the one our test is running on
+			result, err := store.TransferTx(context.Background(), TransferTxParams{
+				FromAccountId: account1.ID,
+				ToAccountId:   account2.ID,
+				Amount:        amount,
+			})
+			transferResults <- result
+			transferResultsError <- err
+		}()
+	}
+
+	//Check for no error in any of the transactions
+	for i := 0; i < noOfTransfers; i++ {
+		err := <-transferResultsError
+		require.NoError(t, err)
+
+		result := <-transferResults
+		require.NotEmpty(t, result)
+
+		//Checking the result
+		//Checking for transfer in result
+		transfer := result.Transfer
+		require.NotEmpty(t, transfer)
+		require.Equal(t, account1.ID, transfer.FromAccountID)
+		require.Equal(t, account2.ID, transfer.ToAccountID)
+		require.Equal(t, amount, transfer.Amount)
+	}
+
 }
